f9b007c21eecce827113804968cc0bbc34277416
Dave86dev <davidochando1986@gmail.com>
2021-05-19 12:10:40 +0200

    Cambios en moviesController

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -10,7 +10,8 @@ class Pelicula {
 
 
     async searchById(id){
-
+        let res = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US`);
+        return res.data;
     }
 
 
